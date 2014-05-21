907b8ce4b7ca555d3a98f9ed78fe8910ce47ab33
Tamim Sookoor <tamimsookoor@gmail.com>
2014-05-21 19:40:52 -0400

    fixed reverse string to work with c-style strings

--- reverse_string.py
+++ reverse_string.py
@@ -1,7 +1,12 @@
+# Code to reverse a C-Style String (e.g. "abcd" is represented as five characters, including the null character.)
+
+
 def reverse_string_reversed(input_string):
-    return "".join(reversed(input_string))
+    return "".join(reversed(input_string[:-1])) + input_string[-1]
 
 def reverse_string_slice(input_string):
-    return input_string[::-1]
+    return input_string[-2::-1] + input_string[-1]
 
-print reverse_string_slice("Madam I'm Adam")
+input_string = "Madam I'm Adam\0"
+
+assert reverse_string_slice(input_string) == reverse_string_reversed(input_string)
